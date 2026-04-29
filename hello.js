af001fc7a5463feb7b3e4793dcfda946db020278
hynh2510-oss <hynh.2510@gmail.com>
2026-04-29 11:11:14 +0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,4 +1,8 @@
 function sayHello(name){
     return "Hello" +"Hanny"+"!";
 }
+function sayGoodbye(name){
+    return "Bye, " + "Hanny";
+}
 console.log(sayHello("Automation"));
+console.log(sayHello("Manual"));
